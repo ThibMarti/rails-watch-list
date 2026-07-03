dc73e41ca4ce90db8e8e80f919e7fd3e89071c95
Thibault Martin <thibaultmartin02@hotmail.com>
2026-07-03 11:33:05 +0200

    Start day 2

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,27 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @movie = Movie.find(params[:bookmark][:movie_id])
+
+    @bookmark.movie = @movie
+    @bookmark.list = @list
+
+    if @bookmark.save
+      redirect_to @list
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+end
